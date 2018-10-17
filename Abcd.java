454a904eda6a43137a557cbd393fffe40e14d92d
FAHIZA1 <fahizafreen@gmail.com>
2018-10-17 15:20:40 +0530

    maven

---
+++ Abcd.java
@@ -0,0 +1,10 @@
+public class Abcd{
+	public static void main(String args[])
+	{
+		System.out.println("main method in Abcd");
+		System.out.println("another message");
+		Abcd abcd=new Abcd();
+	}
+	
+	
+}
